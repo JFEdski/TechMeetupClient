33d0cc90347070440af92bedef4d52e2ed3043ea
keithdavid24 <keithdavid247@gmail.com>
2023-11-29 21:18:21 -0700

    Navbar partial

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <nav className='navbar'>
+
+        <div className='nav-links'>
+          <span className='navbar-title'>Tech Meetup</span>
+          <a href="#explore">Explore</a>
+          <a href="create">Create a new event</a>
+          <a href="#log-in">Login</a>
+          <a href="#sign-up" className='sign-up-btn'>Signup</a>
+        </div>
+
+      </nav>
+    </div >
+
+  );
+}
+
+export default App;
