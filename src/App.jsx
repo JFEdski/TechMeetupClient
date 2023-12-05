b16a426c133d8f56090f96147f631b235e00b8a4
JFEdski <jedelinski@gmail.com>
2023-12-04 18:54:13 -0700

    Google Maps working correctly

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,29 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Navbar from './components/navbar/Navbar';
-import CreateEvent from './components/event/CreateEvent';
-import Login from './components/auth/login/Login';
-import Signup from './components/auth/signup/Signup';
-import Home from './components/Home';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar/Navbar";
+//import CreateEvent from "./components/event/CreateEvent";
+import Login from "./components/auth/login/Login";
+import Signup from "./components/auth/signup/Signup";
+import Home from "./components/Home";
+import EventMap from "./components/map/EventMap";
 // import Explore from './components/Explore';
-import './App.css';
+import "./App.css";
 
 function App() {
-
   return (
-    <div className='App'>
+    <div className="App">
       <BrowserRouter>
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/create" element={<CreateEvent />} />
+          {/* <Route path="/create" element={<CreateEvent />} /> */}
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
         </Routes>
+        <EventMap />
       </BrowserRouter>
     </div>
   );
-};
+}
 
 export default App;
