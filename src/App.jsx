f0cbdafce46342f914ca64140c47ae3ff4204db6
JFEdski <jedelinski@gmail.com>
2023-12-06 18:17:41 -0700

    Committing in order to pull

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
@@ -6,16 +5,7 @@ import CreateEvent from "./components/event/CreateEvent";
 import Login from "./components/auth/login/Login";
 import Signup from "./components/auth/signup/Signup";
 import Home from "./components/Home";
-import EventMap from "./components/map/EventMap";
-
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Navbar from './components/navbar/Navbar';
-import CreateEvent from './components/event/CreateEvent';
-import Login from './components/auth/login/Login';
-import Signup from './components/auth/signup/Signup';
-import Home from './components/Home';
-import Filter from './components/filter/Filter';
+import Filter from "./components/filter/Filter";
 
 // import Explore from './components/Explore';
 import "./App.css";
@@ -32,7 +22,6 @@ function App() {
           <Route path="/signup" element={<Signup />} />
           <Route path="/filter" element={<Filter />} />
         </Routes>
-        <EventMap />
       </BrowserRouter>
     </div>
   );
