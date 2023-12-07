a9ace2ff90619ede5b6a0bd7bb4f9cc11ad8a580
keithdavid24 <keithdavid247@gmail.com>
2023-12-06 19:45:12 -0700

    Event Card mock up

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,10 @@ import CreateEvent from "./components/event/CreateEvent";
 import Login from "./components/auth/login/Login";
 import Signup from "./components/auth/signup/Signup";
 import Home from "./components/Home";
-import EventMap from "./components/map/EventMap";
+// import EventMap from "./components/map/EventMap";
 
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Navbar from './components/navbar/Navbar';
 import Event from './components/event/EventPage';
-import Login from './components/auth/login/Login';
-import Signup from './components/auth/signup/Signup';
-import Home from './components/Home';
 import Filter from './components/filter/Filter';
-
-// import Explore from './components/Explore';
 import "./App.css";
 
 function App() {
@@ -31,8 +23,9 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/filter" element={<Filter />} />
+          <Route path="/create" element={<CreateEvent />} />
         </Routes>
-        <EventMap />
+        {/* <EventMap /> */}
       </BrowserRouter>
     </div>
   );
