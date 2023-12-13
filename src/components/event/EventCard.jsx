f45fe8e1e8639458b18e36325bcd8d9cbd9a4d20
Jared <jedelinski@gmail.com>
2023-12-12 20:23:01 -0700

    Filter exed, Events now showing

--- src/components/event/EventCard.jsx
+++ src/components/event/EventCard.jsx
@@ -1,22 +1,29 @@
 import { Link } from 'react-router-dom';
-
-
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom'
+import EventDetails from './EventDetails';
 
 const EventCard = ({ event }) => {
-
   useEffect(() => {
     console.log(event)
   }, []);
   
   const [events, setEvents] = useState([]);
 
-
+  useEffect(() => {
+    console.log("The events:", events)
+    // Fetch event data from your backend when the component mounts
+    fetch('http://localhost:4000/events/list')
+      .then(response => response.json())
+      .then(data => setEvents(data))
+      .catch(error => console.error('Error fetching events:', error));
+  }, []);
 
   return(
     <div className="event-container" to={<EventDetails/>}>
       <div className="event-item-container">
         <div className="event-img-container">
-          <img src="https://picsum.photos/400/600" alt="Event image"/>
+          {/* <img src="./images/img1.jpg" alt="Event image"/> */}
         </div>
 
         <div className="event-body-container">
@@ -26,16 +33,16 @@ const EventCard = ({ event }) => {
             <div className="separator"></div>
             <p className="event-title">{event.name}</p>
             <p className="event-info">{event.location}</p>
-            <p className="price">{event.date}</p>
+            {/* <p className="price">Free</p> */}
 
             <div className="additional-info">
               <p className="event-info">
                 <i className="fas fa-map-marker-alt"></i>
-                {event.category}
+                {event.language}
               </p>
               <p className="event-info">
                 <i className="far fa-calendar-alt"></i>
-                {event.time}
+                {event.dateTime}
               </p>
               <p className="info description">
                 {event.description}
